d8be0fdecd423851c6a0bbaedf03eeb76f3aaebf
Bri0318 <bmi@macalester.edu>
2025-09-05 10:56:29 -0500

    Create garden.p.py

---
+++ src/ica/01-python+ide/buggy_scripts/garden.p.py
@@ -0,0 +1,7 @@
+bed_len = 10
+bed_width = 2
+ich_per_foot = 12
+flo_inch = 6
+cols = (bed_len * ich_per_foot) / flo_inch
+rows = (bed_width * ich_per_foot) /flo_inch
+print("rows:" , rows, "columns:", cols)
